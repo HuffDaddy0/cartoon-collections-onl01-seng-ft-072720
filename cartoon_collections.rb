9e1c77068b61e97582552fa2ae466f3428040142
Tyler Huffman <ty.huff24@gmail.com>
2020-07-30 14:04:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(dwarves)
   roll_call = []
   dwarves.each_with_index do |dwarf, index|
     roll_call.push("#{index + 1}. #{dwarf}")
-    binding.pry
+    #binding.pry
   end
   roll_call.join("")
 end
